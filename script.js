0e85bb39958bbfb2ee87b6a3ab5c24345303aae9
Negatseg <tseganehnegash@gmail.com>
2023-10-02 21:46:36 -0500

    clear up the code and correct javascript fucntions

--- script.js
+++ script.js
@@ -8,30 +8,30 @@ const weatherCardsDiv = document.querySelector(".weather-cards");
 const API_KEY = "1d9d62ac9684ac5f4f6d51631cc28327";
 
 const createWeatherCard = (cityName, weatherItem, index) => {
-  if (index === 0){
-        return '<div class="details">
-                    <h2>${cityName} (${weatherItem.dx_txt.split(" ")[0]})</h2> 
-                    <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
-                    <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
-                    <h4>Humidity: ${weatherItem.main.humidity} %</h4>
-                </div>
-                <div class="icon">
-                      <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt= "Weather-icon">
-                      <h4>${weatherItem.weather[0].description}</h4>
-                </div>';
-      } else {
-          return '<li class="card">
-                      <h3>(${weatherItem.dx_txt.split(" ")[0]})</h3>
-                      <img src = "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png"  alt= "Weather-icon">
-                      <h4>Temp: ${(weatherItem.main.temp  - 273.15).toFixed(2)}°C</h4>
-                      <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
-                      <h4>Humidity: ${weatherItem.main.humidity} %</h4>
-                  </li>';
+  if (index === 0) {
+    return `<div class="details">
+                <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2> 
+                <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+                <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
+                <h4>Humidity: ${weatherItem.main.humidity} %</h4>
+            </div>
+            <div class="icon">
+                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="Weather-icon">
+                <h4>${weatherItem.weather[0].description}</h4>
+            </div>`;
+  } else {
+    return `<li class="card">
+                <h3>${weatherItem.dt_txt.split(" ")[0]}</h3>
+                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather-icon">
+                <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+                <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
+                <h4>Humidity: ${weatherItem.main.humidity} %</h4>
+            </li>`;
   }
 }
 
-const getWeatherDetails = (cityName, lat,lon) => {
-  const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&&appid=${API_KEY}';
+const getWeatherDetails = (cityName, lat, lon) => {
+  const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
       console.log(data);
@@ -39,7 +39,7 @@ const getWeatherDetails = (cityName, lat,lon) => {
       const uniqueForecastDays = [];
       const fiveDaysForecast = data.list.filter(forecast => {
         const forecastDate = new Date(forecast.dt.txt).getDate();
-        if(uniqueForecastDays.includes(forecastDate)) {
+        if(!uniqueForecastDays.includes(forecastDate)) {
           return uniqueForecastDays.push(forecastDate);
         }
       });
@@ -62,17 +62,17 @@ const getWeatherDetails = (cityName, lat,lon) => {
     });
 }
 
-const getCityCoordiantes = () => {
+const getCityCoordinates = () => {
   const cityName = cityInput.value.trim();
   if (!cityName) return;
-  const GEOCODING_API_URL = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}'
+  const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
   fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
     if(!data.length) return alert('no coordinates found for ${cityName}');
     const {name, lat, lon} = data[0];
     getWeatherDetails(name,lat,lon);
   }).catch(()=>{
-    alert("an error occurred while fetching teh coordiantes");
+    alert("an error occurred while fetching the coordiantes");
   });
 }
 
